7c2c362e63746e9f0384304a921c3c05d9c3d412
solita-jasona <jason.ali@solita.fi>
2022-06-01 15:44:22 +0300

    added db models for topic and message

---
+++ MessageBoardBackend/Message.cs
@@ -0,0 +1,18 @@
+﻿namespace MessageBoardBackend
+{
+	public class Message
+	{
+		public int Id { get; set; }
+
+		public string Contents { get; set; } = string.Empty;
+
+		public DateTime TimeStamp { get; set; } = DateTime.Now;
+
+		public int UserId { get; set; }
+
+		public virtual User User { get; set; }
+
+		
+	}
+}
+
---
+++ MessageBoardBackend/Topic.cs
@@ -0,0 +1,17 @@
+﻿namespace MessageBoardBackend
+{
+	public class Topic
+	{
+		public int Id { get; set; }
+
+		public int Title { get; set; }
+
+		public int MessageCount { get; set; } = 0;
+
+		public virtual Message? LastMessage { get; set; }
+
+		public int LastMessageId { get; set; }
+
+	}
+}
+
