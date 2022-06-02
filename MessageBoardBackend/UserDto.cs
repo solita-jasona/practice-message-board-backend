8f474a19bc523ea1ba5fc2e441fcb177a28473c8
solita-jasona <jason.ali@solita.fi>
2022-06-02 15:39:45 +0300

    created UserDto

---
+++ MessageBoardBackend/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MessageBoardBackend
+{
+	public class UserDto
+	{
+		public string Username { get; set; } = string.Empty;
+
+		public string Password { get; set; } = string.Empty;
+
+		public string UserEmail { get; set; } = string.Empty;
+	}
+}
+
