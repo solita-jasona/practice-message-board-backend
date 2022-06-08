8ba61742de242fe33f0079288845e9324e6a7c3a
solita-jasona <jason.ali@solita.fi>
2022-06-08 18:05:55 +0300

    create message dto

---
+++ MessageBoardBackend/MessageDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MessageBoardBackend
+{
+	public class MessageDto
+	{
+		public int Id { get; set; }
+		public string Contents { get; set; } = string.Empty;
+		public int TopicId { get; set; }
+	}
+}
+
